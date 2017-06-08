634343aa3b8b0176fdd761e4add4c987e0e43a83
Neil <15325061080038@cetis108.edu.mx>
2017-06-08 00:43:00 -0600

    .

--- app/src/main/java/mx/edu/cetis108/cetis108_app005/Archivo.java
+++ app/src/main/java/mx/edu/cetis108/cetis108_app005/Archivo.java
@@ -1,5 +1,21 @@
 package mx.edu.cetis108.cetis108_app005;
 
+import android.util.Log;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Date;
 
 /**
@@ -7,21 +23,86 @@ import java.util.Date;
  */
 
 public class Archivo {
+    String sUrl;
     String usuario;
     String nombre;
     String ruta;
     String tipo;
     Date fechaCreacion;
     Date fechaModificacion;
+
+    InputStream is=null;
+    String result=null;
+    String line=null;
+    int code;
     public Archivo(){
 
     }
-    public Archivo(String usuario, String nombre, String ruta, String tipo, Date fechaCreacion, Date fechaModificacion){
+    public Archivo(String sUrl, String usuario, String nombre, String ruta, String tipo, Date fechaCreacion, Date fechaModificacion){
+        this.sUrl=sUrl;
         this.usuario=usuario;
         this.nombre=nombre;
         this.ruta=ruta;
         this.tipo=tipo;
         this.fechaCreacion=fechaCreacion;
         this.fechaModificacion=fechaModificacion;
+    }
+    public String crearArchivo()
+    {
+        String sRespuesta = "";
+        ArrayList<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+
+        nameValuePairs.add(new BasicNameValuePair("usuario", usuario));
+        nameValuePairs.add(new BasicNameValuePair("nombre", nombre));
+        nameValuePairs.add(new BasicNameValuePair("tipo", tipo));
+        nameValuePairs.add(new BasicNameValuePair("ruta", ruta));
+        try
+        {
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpPost httppost = new HttpPost(sUrl);
+            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+            HttpResponse response = httpclient.execute(httppost);
+            HttpEntity entity = response.getEntity();
+            is = entity.getContent();
+            Log.e("pass 1", "connection success ");
+        }
+        catch(Exception e)
+        {
+            Log.e("Fail 1", e.toString());
+        }
+
+        try
+        {
+            BufferedReader reader = new BufferedReader
+                    (new InputStreamReader(is,"iso-8859-1"),8);
+            StringBuilder sb = new StringBuilder();
+            while ((line = reader.readLine()) != null)
+            {
+                sb.append(line + "\n");
+            }
+            is.close();
+            result = sb.toString();
+            Log.e("pass 2", "connection success ");
+        }
+        catch(Exception e)
+        {
+            Log.e("Fail 2", e.toString());
+        }
+
+        try
+        {
+            JSONObject json_data = new JSONObject(result);
+            code=(json_data.getInt("code"));
+
+            if(code==1)
+            {
+                sRespuesta = "Archivo creado correctamente.";
+            }
+        }
+        catch(Exception e)
+        {
+            Log.e("Fail 3", e.toString());
+        }
+        return sRespuesta;
     }
 }
